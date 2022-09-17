3fc0bd4d68fec70cb1e03442aa0bdd24f4895f15
chickenator <andreych237@gmail.com>
2022-09-17 19:34:27 +0300

    lab2 done

--- Lab2/Lab2.2/src/Main.java
+++ Lab2/Lab2.2/src/Main.java
@@ -4,37 +4,34 @@
 //а Нечипорчук підраховує вартість майна. Потрібно скласти багатопоточний додаток, що моделює діяльність прапорщиків.
 //При вирішенні використати парадигму «виробник-споживач» з активним очікуванням.
 
-import java.util.LinkedList;
-import static java.lang.Math.random;
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class Main {
+
     public static void main(String[] args)
-            throws InterruptedException
-    {
+            throws InterruptedException {
         final Thing thing = new Thing();
+        thing.generateStorage();
 
         Thread ivanov = new Thread(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 try {
                     thing.produce();
-                }
-                catch (InterruptedException e) {
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("ІВАНОВ ВКРАВ");
             }
         });
 
         Thread petrov = new Thread(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 try {
-                    thing.consume();
-                }
-                catch (InterruptedException e) {
+                    thing.produce_consume();
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
@@ -53,50 +50,91 @@ public class Main {
 
         ivanov.start();
         petrov.start();
-        //nechyporenko.start();
+        nechyporenko.start();
 
         ivanov.join();
         petrov.join();
-        //nechyporenko.join();
+        nechyporenko.join();
     }
 
     public static class Thing {
-        LinkedList<Integer> list = new LinkedList<>();
-        int capacity = 1;
+        private final int numberOfThings = 5;
+        private int totalPrice = 0;
 
-        public void produce() throws InterruptedException
-        {
+        private final BlockingQueue<Thing> storage = new ArrayBlockingQueue<Thing>(numberOfThings);
+        private final BlockingQueue<Thing> ivanovToPetrov = new ArrayBlockingQueue<Thing>(numberOfThings);
+        private final BlockingQueue<Thing> truck = new ArrayBlockingQueue<Thing>(numberOfThings);
+        int capacity = 1;
+        int price;
+        Random rand = new Random();
+
+        Thing() {
+            this.price = rand.nextInt(1000);
+        }
+
+        public void generateStorage() throws InterruptedException {
+            for (int i = 0; i < numberOfThings; i++) {
+                storage.put(new Thing());
+            }
+        }
+
+        public int GetPrice() {
+            return price;
+        }
+
+        public void produce() throws InterruptedException {
             int value = 0;
             while (true) {
-                synchronized (this)
-                {
-                    while (list.size() == capacity) {
+                synchronized (this) {
+                    while (ivanovToPetrov.size() == capacity) {
                         wait();
                     }
-                    System.out.println("Іванов вкрав - " + value);
-
-                    list.add(value++);
+                    System.out.println("Іванов вкрав");
+                    ivanovToPetrov.put(storage.take());
 
                     notify();
 
                     Thread.sleep(1000);
+
+                    if (storage.isEmpty()) {
+                        break;
+                    }
                 }
             }
         }
 
-        public void consume() throws InterruptedException
-        {
+        public void produce_consume() throws InterruptedException {
+            int value = 0;
             while (true) {
-                synchronized (this)
-                {
-                    while (list.size() == 0){
+                synchronized (this) {
+                    while (ivanovToPetrov.size() == 0) {
                         wait();
                     }
-                    int val = list.removeFirst();
-                    System.out.println("Петров погрузив-" + val);
+                    System.out.println("Петров погрузив");
+                    truck.put(ivanovToPetrov.take());
+
                     notify();
 
-                    // and sleep
+                    Thread.sleep(1000);
+
+                    if (storage.isEmpty()) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void consume() throws InterruptedException {
+            while (true) {
+                synchronized (this) {
+                    while (truck.isEmpty()) {
+                        wait();
+                    }
+                    totalPrice += truck.take().GetPrice();
+                    System.out.println("Нечипоренко підраховав на сумму: " + totalPrice);
+
+                    notify();
+
                     Thread.sleep(1000);
                 }
             }
